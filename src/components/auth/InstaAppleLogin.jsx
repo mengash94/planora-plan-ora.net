edc09c1913f913a10c9eb1d245cfd290a94f9a9b
מנגשה אטלאי <mengash94@gmail.com>
2025-12-18 09:46:12 +0000

    File changes

--- src/components/auth/InstaAppleLogin.jsx
+++ src/components/auth/InstaAppleLogin.jsx
@@ -77,7 +77,7 @@ export default function InstaAppleLogin() {
         
         await plugin.initialize({
           apple: {
-            clientId: 'net.plan-ora.planora',
+            clientId: 'net.planora.app',
             redirectUrl: 'https://easypalnistaback.firebaseapp.com/__/auth/handler'
           }
         });
